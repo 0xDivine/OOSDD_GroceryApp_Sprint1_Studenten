e13a14ea846aadc868491737512b6bb0a1106f44
Irshad <irshad.nazar-ali@hva.nl>
2025-09-08 17:14:51 +0200

    UC2 finished
    1. Changed few members vars to properties
    2.  Values from properties are passed to constructor

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,15 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId; //To do: change it to a property
-        public int ProductId;     //To do: change it to a property
+        public int GroceryListId { get; set; } // getters en setters
+        public int ProductId { get; set; }     // getters en setters gemaakt
         public int Amount { get; set; }
+
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            GroceryListId = groceryListId; //initalisatie van de properties
+            ProductId = productId;         
+            Amount = amount;               
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
